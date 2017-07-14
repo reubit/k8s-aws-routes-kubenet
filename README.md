cbee80491771288405e766dcc7132ed79ff21ba4
Reuben Mannell <reuben.mannell@siteminder.com>
2017-07-14 12:34:55 +1000

    Initial commit with working Dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # k8s-aws-routes-kubenet
-Updates multiple aws routes tables for use with private topologies when using kubenet.
+Updates multiple aws routes tables for use with private AWS VPC topologies.
+
+For use with kubenet when using multiple route tables in a private topology.
+
+Also disables source/destination checking on k8s instances.
+
+This is an interim solution, pending resolution of: https://github.com/kubernetes/kubernetes/issues/42487
---
+++ k8s-aws-routes-kubenet.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+routes=$(kubectl get nodes -o json | jq '.items | .[] | .spec.podCIDR, .spec.externalID' -r | xargs | sed 's/\([^ ]\+\) \([^ ]\+\) /\1 \2\n/g')
+
+instances_json=$(echo "$routes" | awk '{ print $2 }' | xargs aws ec2 describe-instances --instance-ids)
+
+subnets=$(echo "$instances_json" | jq '.Reservations | .[] | .Instances | .[] | .NetworkInterfaces | .[] | .SubnetId' -r)
+
+tables_json=$(aws ec2 describe-route-tables)
+
+route_tables=$(
+  for each in $subnets; do 
+    echo "$tables_json" | jq '.RouteTables | .[] | select(.Associations | .[] | .SubnetId == "'${each}'") | .RouteTableId' -r
+  done | sort -u
+)
+
+commands=$(
+for table in $route_tables; do
+  OIFS=$IFS; IFS=$'\n'
+  for route in $routes; do
+    cidr=$(echo "$route" | awk '{ print $1}')
+    instance_id=$(echo "$route" | awk '{ print $2}')
+
+    source_dest_check=$(echo "$instances_json" | jq '.Reservations | .[] | .Instances | .[] | select(.InstanceId == "'${instance_id}'") | .SourceDestCheck')
+
+    if [ "$source_dest_check" == "true" ]; then
+      echo "aws ec2 modify-instance-attribute --instance-id ${instance_id} --no-source-dest-check"
+    fi
+
+    check_cidr=$(echo "$tables_json" | jq '.RouteTables | .[] | select(.RouteTableId == "'${table}'") | .Routes | .[] | select(.DestinationCidrBlock == "'${cidr}'") | .DestinationCidrBlock' -r)
+    if [ "$cidr" == "$check_cidr" ]; then
+
+      check_route_instance_id=$(echo "$tables_json" | jq '.RouteTables | .[] | select(.RouteTableId == "'${table}'") | .Routes | .[] | select(.DestinationCidrBlock == "'${cidr}'") | .InstanceId' -r)
+      if [ "$instance_id" != "$check_route_instance_id" ]; then
+        echo "$table $route" | awk '{ print "aws ec2 replace-route --route-table-id " $1 " --destination-cidr-block " $2 " --instance-id " $3}'
+      fi
+
+    else
+      echo "$table $route" | awk '{ print "aws ec2 create-route --route-table-id " $1 " --destination-cidr-block " $2 " --instance-id " $3}'
+    fi
+  done
+  IFS=$OIFS
+done | sort -u
+)
+
+OIFS=$IFS; IFS=$'\n'
+for command in $commands; do
+  echo "$command"
+  if [ "$1" == "--yes" ]; then
+    eval "$command" && echo 'Success!' || echo 'Failed!'
+    echo
+  fi
+done
+IFS=$OIFS
+
